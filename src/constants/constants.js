3b944579695baa996014839dae59b9290583a186
1-aditya-2 <99257070+1-aditya-2@users.noreply.github.com>
2023-11-08 00:08:28 +0530

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,13 +9,22 @@ export const projects = [
     id: 0,
   },
   {
+    title: 'CustomYum',
+    description: "A food recipe website is an online platform that provides a collection of diverse culinary recipes, offering users the opportunity to discover, learn, and prepare a wide range of dishes, catering to various tastes and dietary preferences.",
+    image: '/images/i4.png',
+    tags: ['React', 'Node', 'HTML', 'SCSS', 'JavaScript'],
+    source: 'https://github.com/1-aditya-2/food-recipe',
+    visit: 'https://food-recipe-peach.vercel.app/',
+    id: 1,
+  },
+  {
     title: 'Apoorva',
     description:"This is the official website of Apoova (Techno-Cultural Fest) of our college, IIITK. It tells the timeline of the events, along with other details. Helped in creating this website along with some seniors.Used nextjs, css, javascript to particularly built the Hero and Description Section of the website.",
     image: '/images/i2.png',
     tags: ['NextJs', 'JavaScript',"CSS"],
     source: 'https://github.com/shashank1503-cipher/Apoorv',
-    visit: 'https://google.com',
-    id: 1,
+    visit: 'https://apoorviiitk.co/',
+    id: 2,
   },
   {
     title: 'FlatMate',
@@ -24,17 +33,9 @@ export const projects = [
       tags: ['PHP', 'JavaScript','CSS'],
     source: 'https://github.com/1-aditya-2/FlatMate',
     visit: 'https://google.com',
-    id: 2,
-  },
-  {
-    title: 'GSOC Tagger',
-    description: "Created this blog page which give information regarding GSOC. It tells the importance of GSOC, steps to apply, some insights from its qualifiers. Used normal HTML, CSS, JavaSrcipt for this page.",
-    image: '/images/i4.png',
-    tags: ['HTML', 'CSS', 'JavaScript'],
-    source: 'https://github.com/1-aditya-2/GSOCblog',
-    visit: 'https://1-aditya-2.github.io/GSOCblog/',
     id: 3,
-  },
+  }
+  
 ];
 
 export const TimeLineData = [
